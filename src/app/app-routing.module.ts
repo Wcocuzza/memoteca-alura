4ada04911aa67c887eefa676576e1a368ad0281f
Wallace Cocuzza <wallace.cocuzza@gmail.com>
2023-01-29 21:29:37 -0300

    Adicionado a navegação entre os componentes criar e listar pensamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
+import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'listarPensamento', pathMatch: 'full' },
+  { path: 'criarPensamento', component: CriarPensamentoComponent },
+  { path: 'listarPensamento', component: ListarPensamentoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
