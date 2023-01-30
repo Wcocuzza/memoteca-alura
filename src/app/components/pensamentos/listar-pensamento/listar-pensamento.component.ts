7779387827fd5aaff64a16b50202b5ccd2ce05fe
Wallace Cocuzza <wallace.cocuzza@gmail.com>
2023-01-30 20:57:03 -0300

    Desenvolvido a logica para listar os pensamentos e renderizar o componente 'pensamento'

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent {
-
+  listaPensamentos = [
+    {conteudo: 'Teste1', autoria: 'wcocuzza', modelo: 'modelo1'},
+    {conteudo: 'Teste2', autoria: 'wcocuzza', modelo: 'modelo2'},
+    {conteudo: 'Teste3', autoria: 'wcocuzza', modelo: 'modelo3'},
+    {conteudo: 'Teste3', autoria: 'wcocuzza', modelo: 'modelo3'}
+  ];
 }
